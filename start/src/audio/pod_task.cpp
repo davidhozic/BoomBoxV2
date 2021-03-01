6a56774a62c57e9e7f987d602889bd101fe7c7d1
David Hozic <davidhozic@gmail.com>
2021-03-01 10:06:08 +0100

    Audio system subfunction improvement | STATUS: Ready for testing

--- start/src/audio/pod_task.cpp
+++ start/src/audio/pod_task.cpp
@@ -33,11 +33,8 @@ void Color_Fade_task(void *B) //Fade iz ene barve v drugo
         if (Mixed_fade_control == NULL) // Dihalni nacin svetlosti se ne izvaja
             tr_bright = 255;
 
-        if (Timers.color_timer.vrednost() > 15)
-        {
-            color_fade_funct(&BARVA);
-            Timers.color_timer.ponastavi();
-        }
+        vTaskDelay(16 / portTICK_PERIOD_MS);
+        color_fade_funct(&BARVA);
     }
     color_fade_control = NULL;
     vTaskDelete(NULL);
@@ -45,24 +42,23 @@ void Color_Fade_task(void *B) //Fade iz ene barve v drugo
 
 void Fade_Breathe_Task(void *B)
 {
-    vTaskSuspend(audio_system_control);
 
     tr_r = mozne_barve.barvni_ptr[BARVA][0];
     tr_z = mozne_barve.barvni_ptr[BARVA][1];
     tr_m = mozne_barve.barvni_ptr[BARVA][2];
-    tr_bright = 0;
 
     while (tr_bright < 255)
     {
         svetlost_mod_funct(&tr_bright, 1);
+        vTaskDelay(1);
     }
 
     while (tr_bright > 0)
     {
         svetlost_mod_funct(&tr_bright, -1);
+        vTaskDelay(1);
     }
-    vTaskResume(audio_system_control);
-
+    tr_bright = 0;
     Breathe_control = NULL;
     vTaskDelete(NULL);
 }
@@ -72,7 +68,6 @@ void Mesan_fade_task(void *b)
     delete_AVDIO_subTASK(dont_Mixed_fade_delete);
     xTaskCreate(Fade_Breathe_Task, "breathe fade", 45, b, 1, &Breathe_control);
     xTaskCreate(Color_Fade_task, "col_fade", 45, b, 1, &color_fade_control);
-    delay(100);
     while (Breathe_control != NULL)
     {
     };
