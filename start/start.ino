9d1af40e6e901863fbbe166b67e9bf4d6a1b32aa
David Hozic <davidhozic@gmail.com>
2021-03-05 16:52:04 +0100

    Semi stable version update on FreeRTOS

--- start/start.ino
+++ start/start.ino
@@ -45,7 +45,6 @@ void setup()
   voltage_SEM = xSemaphoreCreateMutex();
   xSemaphoreGive(Thermal_SEM);
   xSemaphoreGive(voltage_SEM);
-
   xTaskCreate(core, "_core", 64, NULL, tskIDLE_PRIORITY, &core_control);
   xTaskCreate(events, "Events task", 80, NULL, 3, &event_control);
   xTaskCreate(zaslon, "LVCHRG", 64, NULL, tskIDLE_PRIORITY, &zaslon_control);
